6d34e8a4fcb7c10d3c917892e6301f70832dfc3e
unknown <yonoraphael@gmail.com>
2020-01-18 21:21:19 +0700

    18 jan 20

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='wraper'>
+      <div id='bg'></div>
+      <div id='overlay'></div>
+        <div id='main'>
+          <header id='header'>
+            <h1>Suyono</h1>
+            <p>Front-End . Web Designer</p>
+          </header>
+        </div>
     </div>
   );
 }

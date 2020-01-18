a581ddd1b228cb75eb2d682dbb5e9d69064ae125
unknown <yonoraphael@gmail.com>
2020-01-19 00:59:37 +0700

    18 jan 20

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {FaFacebook, FaTwitter, FaDribbble, FaGithub, FaEnvelope, FaRegCopyright} from 'react-icons/fa';
 
 function App() {
   return (
@@ -9,8 +10,20 @@ function App() {
         <div id='main'>
           <header id='header'>
             <h1>Suyono</h1>
-            <p>Front-End . Web Designer</p>
+            <h3>Front-End . Web Designer</h3>
+            <nav>
+              <ul>
+                <li><a href='#' target='blank' ><FaFacebook/></a></li>
+                <li><a href='#'><FaTwitter/></a></li>
+                <li><a href='#'><FaDribbble/></a></li>
+                <li><a href='#'><FaGithub/></a></li>
+                <li><a href='#'><FaEnvelope/></a></li>
+              </ul>
+            </nav>
           </header>
+          <footer id='footer'>
+            <p><FaRegCopyright/>LandingPage. Design: Suyono.</p>
+          </footer>
         </div>
     </div>
   );

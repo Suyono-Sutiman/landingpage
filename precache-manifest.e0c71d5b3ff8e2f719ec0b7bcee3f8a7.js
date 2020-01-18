81d6dbe60c30b209c2215b376f951113c8fcc20f
unknown <yonoraphael@gmail.com>
2020-01-19 01:01:52 +0700

    Updates

---
+++ precache-manifest.e0c71d5b3ff8e2f719ec0b7bcee3f8a7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d02aaea08e5469415f4f5a6d6939d3",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "7bc0d6e882ce0e056192",
+    "url": "/landingpage/static/css/main.53235121.chunk.css"
+  },
+  {
+    "revision": "ed40b9d031fc1dfbfcf2",
+    "url": "/landingpage/static/js/2.2b9c4546.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/landingpage/static/js/2.2b9c4546.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bc0d6e882ce0e056192",
+    "url": "/landingpage/static/js/main.658231e7.chunk.js"
+  },
+  {
+    "revision": "1c5ec8016b5612361447",
+    "url": "/landingpage/static/js/runtime-main.4f098a2f.js"
+  },
+  {
+    "revision": "3a1b3910a0456bc3a57ea28930aa7a3b",
+    "url": "/landingpage/static/media/bg1.3a1b3910.jpg"
+  },
+  {
+    "revision": "b79220d57669fb07c51d13830dcb5bd6",
+    "url": "/landingpage/static/media/overlay-pattern.b79220d5.png"
+  },
+  {
+    "revision": "8a09a2d95fc099323903cd88dce48aa6",
+    "url": "/landingpage/static/media/overlay.8a09a2d9.svg"
+  }
+]);
